0d55c057bee39947f8324016d3834eed7c0326c9
aawahanbansal <aawahan.bansal_cs25@gla.ac.in>
2026-06-22 20:58:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+# Load files
+model = joblib.load("model.pkl")
+scaler = joblib.load("scaler.pkl")
+encoders = joblib.load("encoders.pkl")
+
+st.title("Airline Passenger Satisfaction Predictor")
+
+uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
+
+if uploaded_file:
+    df = pd.read_csv(uploaded_file)
+
+    # Apply saved encoders
+    for col in ['Gender', 'Customer Type', 'Type of Travel', 'Class']:
+        df[col] = encoders[col].transform(df[col])
+
+    feature_cols = [
+        'Gender', 'Customer Type', 'Age',
+        'Type of Travel', 'Class', 'Flight Distance',
+        'Inflight wifi service',
+        'Departure/Arrival time convenient',
+        'Ease of Online booking',
+        'Gate location',
+        'Food and drink',
+        'Online boarding',
+        'Seat comfort',
+        'Inflight entertainment',
+        'On-board service',
+        'Leg room service',
+        'Baggage handling',
+        'Checkin service',
+        'Inflight service',
+        'Cleanliness',
+        'Departure Delay in Minutes',
+        'Arrival Delay in Minutes'
+    ]
+
+    X = df[feature_cols]
+
+    X_scaled = scaler.transform(X)
+
+    predictions = model.predict(X_scaled)
+    probabilities = model.predict_proba(X_scaled)[:, 1]
+
+    df["Prediction"] = predictions
+    df["Prediction"] = df["Prediction"].map({
+        0: "Neutral/Dissatisfied",
+        1: "Satisfied"
+    })
+
+    df["Satisfaction Probability"] = probabilities
+
+    st.success("Prediction Complete!")
+    st.dataframe(df)
+
+    csv = df.to_csv(index=False).encode("utf-8")
+
+    st.download_button(
+        "Download Results",
+        csv,
+        "predictions.csv",
+        "text/csv"
+    )
