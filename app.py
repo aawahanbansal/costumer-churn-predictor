c0ea37b3a8780e0069366a663afcb7f7021a5818
aawahanbansal <aawahan.bansal_cs25@gla.ac.in>
2026-06-22 21:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,9 +40,8 @@ if uploaded_file:
     ]
 
     X = df[feature_cols]
-
-    st.write("Missing values:")
-    st.write(X.isnull().sum())
+    df['Arrival Delay in Minutes'] = df['Arrival Delay in Minutes'].fillna(
+    df['Arrival Delay in Minutes'].median())
     X_scaled = scaler.transform(X)
 
     predictions = model.predict(X_scaled)
